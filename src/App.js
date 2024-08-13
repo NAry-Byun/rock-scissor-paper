f7cab57124e91ee613a5d8013cd107b749d6553d
nary <s4036180@student.rmit.edu.au>
2024-08-14 01:02:39 +1000

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-// Remove the unused logo import
-// import logo from './logo.svg';
 import './App.css';
 import Box from './component/box'; // Ensure this matches the actual file name
 
+const choice = {
+  rock: {
+    name: "rock",
+    img: "https://thumb.ac-illust.com/0a/0a682b9a893608f5f8c086129d187dd6_w.jpeg"
+  },
+  scissor: {
+    name: "scissor",
+    img: "https://thumb.ac-illust.com/a2/a235fe14b37fb4656cc10f16859e5c23_w.jpeg"
+  },
+  paper: {
+    name: "paper",
+    img: "https://thumb.ac-illust.com/65/65d8224bddc4a1fb91ef1332cc373fc0_w.jpeg"
+  }
+};
+
 function App() {
+  const play = (userChoice) => {
+    console.log("selected",userChoice);
+  };
+
   return (
-    <div>
-      <Box />
+    <div className="main">
+      <Box title="You" img={choice.rock.img} />
+      <Box title="Computer" img={choice.scissor.img} />
+      <div className="button">
+        <button onClick={() =>play("rock")}>Rock</button>
+        <button onClick={() =>play("scissor")}>Scissor</button>
+        <button onClick={() =>play("paper")}>Paper</button>
+      </div>
     </div>
   );
 }
--- src/component/box.js
+++ src/component/box.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Box = () => {
+const Box = (props) => {
   return (
-    <div>
-      <h1>Box</h1>
-      {/* Provide a valid src or remove the img tag if not needed */}
-      <img src="" alt="Box Image" />
+    <div className="box">
+        <h1>{props.title}</h1>
+      <img 
+        className="item-img" 
+        src="https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/c578d42d-0cc3-4f72-8035-4d57dd9a24de/a0b37cf1-b8a9-4e83-bf4f-889864692138.png" 
+        alt="Box Image" 
+      />
+      <h2>Win</h2>
     </div>
   );
 }
