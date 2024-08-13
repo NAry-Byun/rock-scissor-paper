6d1093b4b069ec2d8a3c18ea4f21e5217ccd7915
nary <s4036180@student.rmit.edu.au>
2024-08-13 23:36:33 +1000

    first commit

---
+++ src/component/box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Box = () => {
+  return (
+    <div>
+      <h1>Box</h1>
+      {/* Provide a valid src or remove the img tag if not needed */}
+      <img src="" alt="Box Image" />
+    </div>
+  );
+}
+
+export default Box;
